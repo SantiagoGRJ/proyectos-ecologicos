bfa631987929aa0cb8f639c32620d7dd470cb908
SantiagoGRJ <santiago-2003-rojas@hotmail.comgit config --global --listclsgit config --global user.email santiago-2003-rojas@hotmail.com>
2024-11-02 17:06:51 -0600

    instalando Livewire al Proyecto

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-/**
- * sadssa
- * a
- */
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
