30261e914a9e7709fb826e5afd176ca3df058e00
SantiagoGRJ <santiago-2003-rojas@hotmail.comgit config --global --listclsgit config --global user.email santiago-2003-rojas@hotmail.com>
2024-11-02 17:55:53 -0600

    Creacion de componentes

---
+++ app/Livewire/Dashboard/InvestorDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class InvestorDashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.investor-dashboard');
+    }
+}
---
+++ app/Livewire/Projects/ProjectApproval.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use Livewire\Component;
+
+class ProjectApproval extends Component
+{
+    public function render()
+    {
+        return view('livewire.projects.project-approval');
+    }
+}
