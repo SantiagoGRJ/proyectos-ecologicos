864616f9e44a6c44da4370d0136071a583d5ddc1
SantiagoGRJ <santiago-2003-rojas@hotmail.comgit config --global --listclsgit config --global user.email santiago-2003-rojas@hotmail.com>
2024-11-02 18:26:42 -0600

    Agregando identificador a las vistas de cada Rol

--- app/Livewire/Dashboard/PushingDashboard.php
+++ app/Livewire/Dashboard/PushingDashboard.php
@@ -3,6 +3,11 @@
 namespace App\Livewire\Dashboard;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
+
+#[Layout('layouts.app')]
+#[Title('Default')]
 
 class PushingDashboard extends Component
 {
--- resources/views/livewire/dashboard/admin-dashboard.blade.php
+++ resources/views/livewire/dashboard/admin-dashboard.blade.php
@@ -1,3 +1,4 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
+    <h1>Admin Dashboard</h1>
 </div>
