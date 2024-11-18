9b76573524460678f555df998bb96f552aadc0f7
SantiagoGRJ <santiago-2003-rojas@hotmail.comgit config --global --listclsgit config --global user.email santiago-2003-rojas@hotmail.com>
2024-11-17 18:50:17 -0600

    Creacion de la vista Acerca de Nosotros

--- resources/views/livewire/general/about-us.blade.php
+++ resources/views/livewire/general/about-us.blade.php
@@ -1,4 +1,9 @@
 <div class="mx-auto max-w-[1300px]">
     {{-- Nothing in the world is as soft and yielding as water. --}}
     <livewire:components.bread-crumb pageName="Contact Page" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+
+
+    <livewire:components.about-section />
+
+    <livewire:layout.footer />
 </div>
