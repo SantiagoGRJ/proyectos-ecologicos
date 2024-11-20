12acd8da26da2733839f86c6bb705860b3345584
SantiagoGRJ <santiago-2003-rojas@hotmail.comgit config --global --listclsgit config --global user.email santiago-2003-rojas@hotmail.com>
2024-11-20 09:16:32 -0600

    Agregando redireccion al Footer

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -25,31 +25,29 @@
             <div class="flex flex-col items-center text-center flex-1">
                 <h2 class="text-lg font-semibold text-green-500 mb-6">Enlaces útiles</h2>
                 <ul class="flex flex-col items-center">
-                    <li><a href="/" class="text-base text-green-400 hover:text-primary mb-4">Home</a></li>
-                    <li><a href="/" class="text-base text-green-400 hover:text-primary mb-4">Sobre Nosotros</a></li>
-                    <li><a href="/" class="text-base text-green-400 hover:text-primary mb-4">Contacto</a></li>
+                    <li><a href="{{ route('home.all') }}" class="text-base text-green-400 hover:text-primary mb-4">Home</a></li>
+                    <li><a href="{{ route('about.all') }}" class="text-base text-green-400 hover:text-primary mb-4">Sobre Nosotros</a>
+                    </li>
+                    <li><a href="{{ route('contact.all') }}" class="text-base text-green-400 hover:text-primary mb-4">Contacto</a></li>
                     {{-- <li><a href="/" class="text-base text-green-400 hover:text-primary mb-4">Términos y Condiciones</a></li>
                     <li><a href="/" class="text-base text-green-400 hover:text-primary">Política de Privacidad</a></li> --}}
                 </ul>
             </div>
 
-            @if (Route::currentRouteName() === 'contact.all')
-
-            @else
-            <!-- Suscripción -->
-            <div class="flex flex-col items-center text-center flex-1">
-                <h2 class="text-lg font-semibold text-green-500 mb-6">Suscríbete</h2>
-                <p class="text-base text-green-400 mb-6">Recibe novedades y actualizaciones sobre proyectos ecológicos y
-                    más.</p>
-                <form action="" method="POST" class="flex flex-col items-center w-full">
-                    <input
-    type="email"
-    class="w-full rounded-md border border-green-400 bg-green-50 py-3 px-6 text-base font-medium text-green-600 placeholder-green-400 shadow-sm outline-none focus:border-green-500 focus:ring-2 focus:ring-green-500 dark:border-green-600 dark:bg-[#242B51] dark:text-green-300 dark:placeholder-green-500 dark:focus:border-green-400 dark:focus:ring-green-400 mb-4"
-    placeholder="Introduce tu correo"
-/>
-                    <button type="submit" class="bg-green-700 text-white py-2 px-6 rounded-lg">Suscribirse</button>
-                </form>
-            </div>
+            @if (!(Route::currentRouteName() === 'contact.all'))
+                <!-- Suscripción -->
+                <div class="flex flex-col items-center text-center flex-1">
+                    <h2 class="text-lg font-semibold text-green-500 mb-6">Suscríbete</h2>
+                    <p class="text-base text-green-400 mb-6">Recibe novedades y actualizaciones sobre proyectos
+                        ecológicos y
+                        más.</p>
+                    <form action="" method="POST" class="flex flex-col items-center w-full">
+                        <input type="email"
+                            class="w-full rounded-md border border-green-400 bg-green-50 py-3 px-6 text-base font-medium text-green-600 placeholder-green-400 shadow-sm outline-none focus:border-green-500 focus:ring-2 focus:ring-green-500 dark:border-green-600 dark:bg-[#242B51] dark:text-green-300 dark:placeholder-green-500 dark:focus:border-green-400 dark:focus:ring-green-400 mb-4"
+                            placeholder="Introduce tu correo" />
+                        <button type="submit" class="bg-green-700 text-white py-2 px-6 rounded-lg">Suscribirse</button>
+                    </form>
+                </div>
             @endif
         </div>
     </div>
